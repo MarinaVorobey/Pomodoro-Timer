f08864e257c8196afc346a8a29b2252ada3ba768
Marina Vorobey <phoenix191999@gmail.com>
2024-04-21 17:43:37 +0300

    feat: home page layout complete

--- Project/src/ui/Icon/icons/Plus.tsx
+++ Project/src/ui/Icon/icons/Plus.tsx
@@ -10,8 +10,7 @@ export function Plus() {
       <circle /* colored in C4 */ cx="25" cy="25" r="25" fill="none" />
       <path
         d="M26.2756 26.1321V33H23.7244V26.1321H17V23.7029H23.7244V17H26.2756V23.7029H33V26.1321H26.2756Z"
-        /* colored in white */
-        fill="none"
+        fill="#FFFFFF"
       />
     </svg>
   );
